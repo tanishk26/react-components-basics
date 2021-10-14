d21bf296b00ed3b8a3f9963c8db10b39f08ec671
Tanishk Thilakan <53421250+tanishk26@users.noreply.github.com>
2021-10-14 20:01:13 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import List from "./List.jsx";
+import Heading from "./Heading.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+//first letter of the function name has to be in caps
+function Heading() {
+  return <h1> My Favourite Foods </h1>;
+}
+
+export default Heading;
+//dont use () after Heading as it immediately returns the value
